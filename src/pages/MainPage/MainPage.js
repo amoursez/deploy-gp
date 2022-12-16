dda361b14fc92ecfc2fab854a92ca322744fa861
Sezim <sezim.ydyrysova@alatoo.edu>
2022-12-16 16:10:03 +0600

    errors fix

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -5,12 +5,11 @@ import BigNewsImage from '../../assets/main/newsImage.png'
 import { FieldTimeOutlined } from '@ant-design/icons'
 import './style.scss';
 import line from '../../assets/main/line.png';
-import EntryPage from '../EntryPage/EntryPage'
+import AboutUsPage from '../AboutUsPage/AboutUsPage'
 
 const MainPage = () => {
   return (
     <div>
-      <EntryPage />
     <div className='section  container'>
      <div className='news'>
       <h3>ПОСЛЕДНИЕ НОВОСТИ</h3>
@@ -39,6 +38,7 @@ const MainPage = () => {
       </div>
      </div>
     </div>
+    <AboutUsPage/>
     </div>
   )
 }
